0ce6a6dcbfac6ea0ce72d0eedc1b225745a9a631
Masha <Masha>
2016-11-03 15:48:11 +0300

    BubbleSort refactoring

--- Task2.Tests/NonRectangularArrayTests.cs
+++ Task2.Tests/NonRectangularArrayTests.cs
@@ -1,162 +1,117 @@
 ﻿using System;
+using System.Collections;
+using System.Linq;
 using NUnit.Framework;
+using static Task2.NonRectangularArray;
 
 namespace Task2.Tests
 {
+    public class RowsSumIncrease : IArrayComparer
+    {
+        public int Compare(int[] arr1, int[] arr2)
+        {
+            return arr1.Sum() - arr2.Sum();
+        }
+    }
+
+    public class RowsSumDecrease : IArrayComparer
+    {
+        public int Compare(int[] arr1, int[] arr2)
+        {
+            return arr2.Sum() - arr1.Sum();
+        }
+    }
+
+    public class RowsMaxElementIncrease : IArrayComparer
+    {
+        public int Compare(int[] arr1, int[] arr2)
+        {
+            return arr1.Max() - arr2.Max();
+        }
+    }
+
+    public class RowsMaxElementDecrease : IArrayComparer
+    {
+        public int Compare(int[] arr1, int[] arr2)
+        {
+            return arr2.Max() - arr1.Max();
+        }
+    }
 
     [TestFixture]
     public class NonRectangularArrayTests
     {
-        [Test]
-        public void BubbleSortRowsSumIncrease()
+        private static RowsSumIncrease rowsSumIncrease = new RowsSumIncrease();
+        private static RowsSumDecrease rowsSumDecrease = new RowsSumDecrease();
+        private static RowsMaxElementIncrease rowsMaxElementIncrease
+            = new RowsMaxElementIncrease();
+        private static RowsMaxElementDecrease rowsMaxElementDecrease
+            = new RowsMaxElementDecrease();
+
+        private static int[][] source1 = new[]
         {
-            var arr = new[]
-            {
-                new[] {1, 2, 3, 4},
-                new[] {5, 6},
-                new[] {7},
-                new[] {8, 9, 0}
-            };
+            new[] {1, 2, 3, 4},
+            new[] {5, 6},
+            new[] {7},
+            new[] {8, 9, 0}
+        };
+        private static int[][] source2 = new[]
+        {
+            new[] {3, 18, 5, 5},
+            new[] {4},
+            new[] {0, 2, 1},
+            new[] {15, 2, 3, 9, 10}
+        };
+        private static int[][] source3  = new int[][] { };
+        private static int[][] expected1 = new[]
+        {
+            new[] {7},
+            new[] {1, 2, 3, 4},
+            new[] {5, 6},
+            new[] {8, 9, 0}
+        };
+        private static int[][] expected2 = new[]
+        {
+            new[] {8, 9, 0},
+            new[] {5, 6},
+            new[] {1, 2, 3, 4},
+            new[] {7}
+        };
+        private static int[][] expected3 = new[]
+        {
+            new[] {0, 2, 1},
+            new[] {4},
+            new[] {15, 2, 3, 9, 10},
+            new[] {3, 18, 5, 5}
+        };
+        private static int[][] expected4 = new[]
+        {
+            new[] {3, 18, 5, 5},
+            new[] {15, 2, 3, 9, 10},
+            new[] {4},
+            new[] {0, 2, 1}
+        };
 
-            var source = NonRectangularArray.BubbleSort(arr, SortMethod.RowsSum, SortOrder.Increase);
-
-            var expected = new[]
-            {
-                new[] {7},
-                new[] {1, 2, 3, 4},
-                new[] {5, 6},
-                new[] {8, 9, 0}
-            };
-
-            Assert.AreEqual(source, expected);
+        [Test, TestCaseSource(typeof(BubbleSortTestClass), nameof(BubbleSortTestClass.TestCases))]
+        public int[][] BubbleSortTests(int[][] arr, IArrayComparer iArrayComparer)
+        {
+            return BubbleSort(arr, iArrayComparer);
         }
 
-        [Test]
-        public void BubbleSortRowsSumDecrease()
+        public class BubbleSortTestClass
         {
-            var arr = new[]
+            public static IEnumerable TestCases
             {
-                new[] {1, 2, 3, 4},
-                new[] {5, 6},
-                new[] {7},
-                new[] {8, 9, 0}
-            };
-
-            var source = NonRectangularArray.BubbleSort(arr, SortMethod.RowsSum, SortOrder.Decrease);
-
-            var expected = new[]
-            {
-                new[] {8, 9, 0},
-                new[] {5, 6},
-                new[] {1, 2, 3, 4},
-                new[] {7}
-            };
-
-            Assert.AreEqual(source, expected);
-        }
-
-        [Test]
-        public void BubbleSortRowsMaxElementIncrease()
-        {
-            var arr = new[]
-            {
-                new[] {3, 18, 5, 5},
-                new[] {4},
-                new[] {0, 2, 1},
-                new[] {15, 2, 3, 9, 10}
-            };
-
-            var source = NonRectangularArray.BubbleSort(arr, SortMethod.RowsMaxElements, SortOrder.Increase);
-
-            var expected = new[]
-            {
-                new[] {0, 2, 1},
-                new[] {4},
-                new[] {15, 2, 3, 9, 10},
-                new[] {3, 18, 5, 5}
-            };
-
-            Assert.AreEqual(source, expected);
-        }
-
-        [Test]
-        public void BubbleSortRowsMaxElementDecrease()
-        {
-            var arr = new[]
-            {
-                new[] {3, 18, 5, 5},
-                new[] {4},
-                new[] {0, 2, 1},
-                new[] {15, 2, 3, 9, 10}
-            };
-
-            var source = NonRectangularArray.BubbleSort(arr, SortMethod.RowsMaxElements, SortOrder.Decrease);
-
-            var expected = new[]
-            {
-                new[] {3, 18, 5, 5},
-                new[] {15, 2, 3, 9, 10},
-                new[] {4},
-                new[] {0, 2, 1}
-            };
-
-            Assert.AreEqual(source, expected);
-        }
-
-        [Test]
-        public void BubbleSortRowsMinElementIncrease()
-        {
-            var arr = new[]
-            {
-                new[] {3, 18, 5, 5},
-                new[] {4},
-                new[] {0, 2, 1},
-                new[] {15, 2, 3, 9, 10}
-            };
-
-            var source = NonRectangularArray.BubbleSort(arr, SortMethod.RowsMinElements, SortOrder.Increase);
-
-            var expected = new[]
-            {
-                new[] {0, 2, 1},
-                new[] {15, 2, 3, 9, 10},
-                new[] {3, 18, 5, 5},
-                new[] {4}
-            };
-
-            Assert.AreEqual(source, expected);
-        }
-
-        [Test]
-        public void BubbleSortRowsMinElementDecrease()
-        {
-            var arr = new[]
-            {
-                new[] {3, 18, 5, 5},
-                new[] {4},
-                new[] {0, 2, 1},
-                new[] {15, 2, 3, 9, 10}
-            };
-
-            var source = NonRectangularArray.BubbleSort(arr, SortMethod.RowsMinElements, SortOrder.Decrease);
-
-            var expected = new[]
-            {
-                new[] {4},
-                new[] {3, 18, 5, 5},
-                new[] {15, 2, 3, 9, 10},
-                new[] {0, 2, 1}
-            };
-
-            Assert.AreEqual(source, expected);
-        }
-
-        [Test]
-        [ExpectedException(typeof(ArgumentOutOfRangeException))]
-        public void BubbleSortException()
-        {
-            var arr = new int[][]{};
-            var source = NonRectangularArray.BubbleSort(arr, SortMethod.RowsSum, SortOrder.Increase);
+                get
+                {
+                    yield return new TestCaseData(source1, rowsSumIncrease).Returns(expected1);
+                    yield return new TestCaseData(source1, rowsSumDecrease).Returns(expected2);
+                    yield return new TestCaseData(source2, rowsMaxElementIncrease).Returns(expected3);
+                    yield return new TestCaseData(source2, rowsMaxElementDecrease).Returns(expected4);
+                    yield return new TestCaseData(source3, rowsSumIncrease)
+                        .Throws(typeof (ArgumentOutOfRangeException));
+                }
+            }
         }
     }
 }
--- Task2/NonRectangularArray.cs
+++ Task2/NonRectangularArray.cs
@@ -2,39 +2,14 @@
 
 namespace Task2
 {
-    /// <summary>
-    /// Different methods for a bubble sort.
-    /// </summary>
-    public enum SortMethod
+    public interface IArrayComparer
     {
-        RowsSum,
-        RowsMaxElements,
-        RowsMinElements
-    };
-
-    /// <summary>
-    /// Sort order of elements.
-    /// </summary>
-    public enum SortOrder
-    {
-        Increase,
-        Decrease
-    };
+        int Compare(int[] arr1, int[] arr2);
+    }
 
     public class NonRectangularArray
     {
-        /// <summary>
-        /// Sorts a non rectangular integer array with
-        /// a specified sorting method in
-        /// increasing or decreasing order.
-        /// </summary>
-        /// <exception cref="NullReferenceException">Thrown if an array is null.</exception>
-        /// <exception cref="ArgumentOutOfRangeException">Thrown if an array is empty.</exception>
-        /// <param name="arr">An array for handling.</param>
-        /// <param name="sortMethod">A sorting method.</param>
-        /// <param name="sortOrder">A sorting order.</param>
-        /// <returns>Returns a new sorted array.</returns>
-        public static int[][] BubbleSort(int[][] arr, SortMethod sortMethod, SortOrder sortOrder)
+        public static int[][] BubbleSort(int[][] arr, IArrayComparer iArrayComparer)
         {
             if (arr == null)
             {
@@ -47,39 +22,21 @@ namespace Task2
             }
 
             var indexes = new int[arr.Length];
-            var arrInfo = new int[arr.Length];
 
-            switch (sortMethod)
+            for (int i = 0; i < arr.Length; i++)
             {
-                case SortMethod.RowsSum:
-                    Sums(arr, indexes, arrInfo);
-                    break;
-                case SortMethod.RowsMaxElements:
-                    MaxElements(arr, indexes, arrInfo);
-                    break;
-                case SortMethod.RowsMinElements:
-                    MinElements(arr, indexes, arrInfo);
-                    break;
+                indexes[i] = i;
             }
 
-            for (var i = 0; i < arr.Length; i++)
+            for (int i = 0; i < arr.Length; i++)
             {
-                for (var j = 0; j < arr.Length - i - 1; j++)
+                for (int j = 0; j < arr.Length - i - 1; j++)
                 {
-                    switch (sortOrder)
+                    var index1 = indexes[j];
+                    var index2 = indexes[j + 1];
+                    if (iArrayComparer.Compare(arr[index1], arr[index2]) > 0)
                     {
-                        case SortOrder.Increase:
-                            if (arrInfo[j] > arrInfo[j + 1])
-                            {
-                                SwapIndexes(indexes, arrInfo, j, j + 1);
-                            }
-                            break;
-                        case SortOrder.Decrease:
-                            if (arrInfo[j] < arrInfo[j + 1])
-                            {
-                                SwapIndexes(indexes, arrInfo, j, j + 1);
-                            }
-                            break;
+                        SwapIndexes(indexes, j, j + 1);
                     }
                 }
             }
@@ -87,98 +44,13 @@ namespace Task2
             return SortedArray(arr, indexes);
         }
 
-        /// <summary>
-        /// Initialise auxiliary arrays of row indexes and row sums.
-        /// </summary>
-        /// <param name="arr">An array for handling.</param>
-        /// <param name="indexes">An auxiliary array of row indexes.</param>
-        /// <param name="sums">An auxiliary array of row sums.</param>
-        private static void Sums(int[][] arr, int[] indexes, int[] sums)
+        public static void SwapIndexes(int[] indexes, int number1, int number2)
         {
-            for (var i = 0; i < arr.Length; i++)
-            {
-                indexes[i] = i;
-                sums[i] = 0;
-
-                for (var j = 0; j < arr[i].Length; j++)
-                {
-                    sums[i] += arr[i][j];
-                }
-            }
-        }
-
-        /// <summary>
-        /// Initialise auxiliary arrays of row indexes and maximal elements in rows.
-        /// </summary>
-        /// <param name="arr">An array for handling.</param>
-        /// <param name="indexes">An auxiliary array of row indexes.</param>
-        /// <param name="maxElements">An auxiliary array of maximal elements in rows.</param>
-        private static void MaxElements(int[][] arr, int[] indexes, int[] maxElements)
-        {
-            for (var i = 0; i < arr.Length; i++)
-            {
-                indexes[i] = i;
-                maxElements[i] = arr[i][0];
-
-                for (var j = 1; j < arr[i].Length; j++)
-                {
-                    if (arr[i][j] > maxElements[i])
-                    {
-                        maxElements[i] = arr[i][j];
-                    }
-                }
-            }
-        }
-
-        /// <summary>
-        /// Initialise auxiliary arrays of row indexes and minimal elements in rows.
-        /// </summary>
-        /// <param name="arr">An array for handling.</param>
-        /// <param name="indexes">An auxiliary array of row indexes.</param>
-        /// <param name="minElements">An auxiliary array of minimal elements in rows.</param>
-        private static void MinElements(int[][] arr, int[] indexes, int[] minElements)
-        {
-            for (var i = 0; i < arr.Length; i++)
-            {
-                indexes[i] = i;
-                minElements[i] = arr[i][0];
-
-                for (var j = 1; j < arr[i].Length; j++)
-                {
-                    if (arr[i][j] < minElements[i])
-                    {
-                        minElements[i] = arr[i][j];
-                    }
-                }
-            }
-        }
-
-        /// <summary>
-        /// Swaps specified indexes.
-        /// </summary>
-        /// <param name="indexes">An array of row indexes.</param>
-        /// <param name="arrInfo">An array of row sums,
-        /// maximal elements in rows or minimal elements in rows.</param>
-        /// <param name="number1">The first index.</param>
-        /// <param name="number2">The second index.</param>
-        private static void SwapIndexes(int[] indexes, int[] arrInfo, int number1, int number2)
-        {
-            var tmp = indexes[number1];
+            int tmp = indexes[number1];
             indexes[number1] = indexes[number2];
             indexes[number2] = tmp;
-
-            tmp = arrInfo[number1];
-            arrInfo[number1] = arrInfo[number2];
-            arrInfo[number2] = tmp;
         }
 
-        /// <summary>
-        /// Creates a new array based on an old array
-        /// and a specified order of rows.
-        /// </summary>
-        /// <param name="oldArr">A base array.</param>
-        /// <param name="indexes">A specified order of rows.</param>
-        /// <returns>Returns a new sorted array.</returns>
         private static int[][] SortedArray(int[][] oldArr, int[] indexes)
         {
             var newArr = new int[oldArr.Length][];
