6dc7ab0523fe6c295f5ec21437353abf97fdd78a
Masha <Masha>
2016-11-03 16:04:15 +0300

    BubbleSort refactoring

--- Task2/NonRectangularArray.cs
+++ Task2/NonRectangularArray.cs
@@ -2,6 +2,9 @@
 
 namespace Task2
 {
+    /// <summary>
+    /// An interface for comparing two arrays.
+    /// </summary>
     public interface IArrayComparer
     {
         int Compare(int[] arr1, int[] arr2);
@@ -9,6 +12,14 @@ namespace Task2
 
     public class NonRectangularArray
     {
+        /// <summary>
+        /// Sorts a non rectangular array in a specified order.
+        /// </summary>
+        /// <exception cref="NullReferenceException">Thrown if data is null.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown if an array is empty.</exception>
+        /// <param name="arr">An array for handling.</param>
+        /// <param name="iArrayComparer">An interface for row comparison.</param>
+        /// <returns>Returns a new sorted array.</returns>
         public static int[][] BubbleSort(int[][] arr, IArrayComparer iArrayComparer)
         {
             if (arr == null)
@@ -44,6 +55,12 @@ namespace Task2
             return SortedArray(arr, indexes);
         }
 
+        /// <summary>
+        /// Swaps two indexes of an array.
+        /// </summary>
+        /// <param name="indexes">An array for handling.</param>
+        /// <param name="number1">The first index.</param>
+        /// <param name="number2">The second index.</param>
         public static void SwapIndexes(int[] indexes, int number1, int number2)
         {
             int tmp = indexes[number1];
@@ -51,6 +68,13 @@ namespace Task2
             indexes[number2] = tmp;
         }
 
+        /// <summary>
+        /// Creates a new sorted array based on an old array and 
+        /// an order of indexes.
+        /// </summary>
+        /// <param name="oldArr">An old array.</param>
+        /// <param name="indexes">An array of indexes.</param>
+        /// <returns>Returns a new sorted array.</returns>
         private static int[][] SortedArray(int[][] oldArr, int[] indexes)
         {
             var newArr = new int[oldArr.Length][];
